caa520c8dc8a0f146402d9e912b4a2511bdeb8f5
geom9876q-prog <geom9876q@gmail.com>
2026-08-25 22:55:30 +0530

    Implement user registration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,38 @@
 const express= require('express');
+const bcrypt = require("bcrypt");
+const db = require('../config/db');
 
 const router= express.Router();
 
-router.post('/register',(req,res) => {
+router.post('/register', async(req,res) => {
 
     const name=req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
-    res.json({
-        "message": "user registered successfully",
-        "name": name,
-        "email":email,
-        "password": password
-    })
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+         const result = await db.query(
+            "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email",
+            [name, email, hashedPassword]
+        );
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user: result.rows[0]
+        });
+
+    } catch (error) {
+
+        console.error(error.message);
+
+        res.status(500).json({
+            message: "Registration failed"
+        });
+    
+    }
+
 });
 
 module.exports = router;
