dd93ee87c17bab68ca932b91c31423bf1b2dce81
geom9876q-prog <geom9876q@gmail.com>
2026-08-30 10:53:59 +0530

    Add complete course CRUD APIs

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -51,4 +51,69 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.put("/:id", async (req, res) => {
+
+    const  id  = req.params.id;
+    const { title, description } = req.body;
+
+    try {
+
+        const result = await db.query(
+            "UPDATE courses SET title = $1, description = $2 WHERE id = $3 RETURNING *",
+            [title, description, id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Course not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Course updated successfully",
+            course: result.rows[0]
+        });
+
+    } catch (error) {
+
+        console.error(error.message);
+
+        res.status(500).json({
+            message: "Failed to update course"
+        });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+
+        const result = await db.query(
+            "DELETE FROM courses WHERE id = $1 RETURNING *",
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Course not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Course deleted successfully",
+            course: result.rows[0]
+        });
+
+    } catch (error) {
+
+        console.error(error.message);
+
+        res.status(500).json({
+            message: "Failed to delete course"
+        });
+    }
+});
+
 module.exports = router;
